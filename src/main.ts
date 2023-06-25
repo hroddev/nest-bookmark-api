dd00733b130cad638f9aa2d8d7b313024476aa66
Hector Rodriguez <hector@hrod.dev>
2023-06-25 16:54:22 -0500

    Install Swagger docs

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,23 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 import { ValidationPipe } from '@nestjs/common'
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
+
+  // swagger config
+  const options = new DocumentBuilder()
+    .setTitle('Bookmarks CRUD')
+    .setDescription('The bookmarks API with test e2e')
+    .setVersion('1.0')
+    .addTag('bookmarks')
+    .build()
+
+  const document = SwaggerModule.createDocument(app, options)
+  
+  SwaggerModule.setup('api', app, document)
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
